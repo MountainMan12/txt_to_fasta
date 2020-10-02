396f1ab34befba42f916c83e96399dd6b207972f
MountainMan12 <pawan12394@gmail.com>
2020-10-02 11:38:31 +0530

    Added Header Fields in .fasta output file

--- plant_exon.py
+++ plant_exon.py
@@ -6,12 +6,14 @@ import pandas as pd
 df = pd.read_csv('exons_plants.csv', header = 0)
 fh = open('exons_seq.fasta','w') #Write to a .fasta file
 
-
+cols = list(df.columns)
+del cols[0]
+print(cols)
 #Generating a fasta file using columns 2-15 as header info
 for i in range(0, len(df.index)):
     header = list(df.iloc[df.index[i],1:len(df.columns)]) 
     #All header elements are seprated by '_' 
-    fh.write('>'+'_'.join(str(x) for x in header)+'\n')
+    fh.write('>'+''.join(str(x)+'='+str(y)+',' for x,y in zip(cols,header))+'\n')
     #Write FASTA sequence
     fh.write(str(df.iloc[df.index[i], 0])+'\n')
 
