f9c03b941ccbc67aa03b00a32a80063b580ae528
MountainMan12 <pawan12394@gmail.com>
2020-10-02 11:40:30 +0530

    Removed print statement

--- plant_exon.py
+++ plant_exon.py
@@ -7,8 +7,7 @@ df = pd.read_csv('exons_plants.csv', header = 0)
 fh = open('exons_seq.fasta','w') #Write to a .fasta file
 
 cols = list(df.columns)
-del cols[0]
-print(cols)
+del cols[0] #First element not required 
 #Generating a fasta file using columns 2-15 as header info
 for i in range(0, len(df.index)):
     header = list(df.iloc[df.index[i],1:len(df.columns)]) 
